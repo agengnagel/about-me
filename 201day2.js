abb09b4c21c5f5ef1b8a75e4f9cad850edff8738
agengnagel <adriennegengnagel@hotmail.com>
2016-02-10 09:20:42 -0800

    made About me

---
+++ 201day2.js
@@ -0,0 +1,52 @@
+var userName = prompt('What is your name?');
+console.log('The user said their name is ' + userName);
+alert ('Hello '+ userName + ' lets see how well you know Adrienne. '+ 'Please answer Y or N to the following questions');
+
+var answer1 = prompt('Was Adrienne born in California?').toUpperCase();
+if (answer1 === "Y") {
+  alert('Good job!');
+  console.log (userName + ' answered '+ answer1);
+} else if (answer1=== "N") {
+  alert ('Sorry, she was, indeed, born in California');
+  console.log (userName + ' answered ' + answer1);
+}else {
+  alert ('Please follow directions');
+}
+
+var answer2 = prompt('Is Adrienne over 100 years old?').toUpperCase();
+if (answer2 === 'Y') {
+  alert ('You must be crazy.');
+  console.log(userName + ' answered ' +answer2);
+} else if (answer2 === 'N') {
+  alert ('Correct. She is 24');
+  console.log(userName +' answered '+ answer2);
+}else {
+  alert ('Please make sure you are following directions');
+}
+
+var answer3 = prompt('Did Adrienne attend an University of Washington?') .toUpperCase();
+if (answer3 === 'Y') {
+  alert ('Heck no, GO COUGS');
+  console.log(userName + ' answered '+ answer3);
+} else if (answer3 === 'N') {
+  alert ('Correct!');
+}
+  console.log(userName+ ' answered '+answer3);
+
+var answer4 =prompt ('Has Adrienne been working as a coder for 10 years?') .toUpperCase();
+if (answer4 === "Y") {
+    alert('No, silly');
+    console.log(userName +' answered '+answer4)
+} else if (answer4 === "N") {
+  alert ('You are correct');
+  console.log(userName+ ' answered '+answer4)
+}
+
+var answer5 = prompt('Does Adrienne currently want to pursue Ruby on Rails?') .toUpperCase();
+if (answer5 === 'Y') {
+  alert ('Not as of now, but maybe in the future');
+  console.log(userName + ' answered '+ answer5);
+} else if (answer5 === 'N') {
+  alert ('Correct! She wants to learn JavaScript');
+}
+  console.log(userName+ ' answered '+answer5);
